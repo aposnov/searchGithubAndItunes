acaf55096223d6247f85ef0cb91f766e7070a569
Andrey Posnov <mailbox@aposnov.ru>
2019-05-26 17:06:52 +0300

    test init

--- searchGithubAndItunes/ViewController.swift
+++ searchGithubAndItunes/ViewController.swift
@@ -8,12 +8,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+   
 
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var sourceType = 0
+    
+    @IBAction func chooseSource(_ sender: Any) {
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            sourceType = 0
+            print(sourceType)
+//            historyView.isHidden = true
+//            popularView.isHidden = false
+        case 1:
+//            historyView.isHidden = false
+//            popularView.isHidden = true
+            sourceType = 1
+            print(sourceType)
+        default:
+            break;
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let nib = UINib(nibName: "iTunesCell", bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: "iTunesCell")
+        
+       
+        
+        
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "iTunesCell", for: indexPath) as! iTunesCell
+        
+        cell.author.text = "John"
+        cell.name.text = "Hello world"
+        return cell
+    }
+    
+    
 
 
 }
---
+++ searchGithubAndItunes/iTunesCell.swift
@@ -0,0 +1,30 @@
+//
+//  iTunesCell.swift
+//  searchGithubAndItunes
+//
+//  Created by Andrey Posnov on 26/05/2019.
+//  Copyright © 2019 Andrey Posnov. All rights reserved.
+//
+
+import UIKit
+
+class iTunesCell: UITableViewCell, UITextFieldDelegate {
+    
+    @IBOutlet weak var icon: UIImageView!
+    @IBOutlet weak var author: UILabel!
+    @IBOutlet weak var name: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+
+    
+
+}
